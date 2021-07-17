ef1f8337bdc04660052b394052a06915d863ddb3
oana-rancov <oanamariarancov@yahoo.com>
2021-07-17 21:22:11 +0300

    Styles + ActionBar

--- app/src/main/java/com/example/androidfundamentals/MainActivity7.java
+++ app/src/main/java/com/example/androidfundamentals/MainActivity7.java
@@ -3,6 +3,7 @@ package com.example.androidfundamentals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity7 extends AppCompatActivity {
 
@@ -10,5 +11,25 @@ public class MainActivity7 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main7);
+
+        //BACK BUTTON ACTION BAR
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+
+    //Asta pentru navigation drawer alea cu 3 puncte-> cand ne cream un menu in action bar
+    /*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    } */
+
+
 }
